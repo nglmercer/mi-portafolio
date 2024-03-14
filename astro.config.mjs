1f980d10196c634a24a6f86b5bd2acdf191c8f02
melserngl <128845117+nglmercer@users.noreply.github.com>
2024-03-14 05:44:07 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nglmercer.github.io',
-    base: '/',
+    base: '/mi-portafolio',
+    trailingSlash: 'ignore',
 });
